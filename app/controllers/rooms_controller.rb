5fd038f79c22d0f07afb7161fb6f555db3c1100f
vagrant <vagrant@localhost.localdomain>
2020-09-28 14:40:33 +0000

    add_fix_dm

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -18,7 +18,22 @@ class RoomsController < ApplicationController
    end
 
   def index
-   @rooms =  current_user.rooms
+     user_all = current_user.following_user & current_user.follower_user
+     current_user_entries = Entry.where(user_id: current_user.id)
+     # ユーザの持っているエントリーidを持ってきている
+     follower_entries = Entry.where(user_id: user_all.map{|n| n.id})
+     # 相互フォローしている相手のエントリーidを全て持ってきている
+     logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+     # テスト
+     logger.debug(current_user_entries.map{|n| n.room_id})
+     logger.debug(follower_entries.map{|n| n.room_id})
+     logger.debug(current_user_entries.map{|n| n.room_id} & follower_entries.map{|n| n.room_id})
+     logger.debug(Entry.all.map{|n| "user_id: #{n.user_id}, room_id: #{n.room_id}\n"})
+     logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+     # テスト
+     @rooms = Room.where(id: current_user_entries.map{|n| n.room_id} & follower_entries.map{|n| n.room_id})
+     # ログインしているuserのroom_id&相互フォローの相手のroom_id
+     # &積集合になる
   end
   def room
   	@user = User.find(params[current_user])
